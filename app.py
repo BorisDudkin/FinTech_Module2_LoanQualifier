da2943e31b774fbfb9ec1cbcca4d1d233c836915
Boris Dudkin <boris.dudkin@gmail.com>
2022-12-01 22:08:59 -0500

    save_qualifying_loans function description augmented by adding a list of conditions

--- app.py
+++ app.py
@@ -99,7 +99,12 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
 
 def save_qualifying_loans(qualifying_loans):
-    """Saves the qualifying loans to a CSV file.
+    """Saves the qualifying loans to a CSV file when certain conditions are met:
+    - Given that I am using the loan qualifier CLI, when I run the qualifier, then the tool should prompt the user to save the results as a CSV file.
+    - Given that no qualifying loans exist, when prompting a user to save a file, then the program should notify the user and exit.
+    - Given that I have a list of qualifying loans, when I am prompted to save the results, then I should be able to opt out of saving the file.
+    - Given that I have a list of qualifying loans, when I choose to save the loans, the tool should prompt for a file path to save the file.
+    - Given that I am using the loan qualifier CLI, when I choose to save the loans, then the tool should save the results as a CSV file.
 
     Args:
         qualifying_loans (list of lists): The qualifying bank loans.
@@ -123,11 +128,8 @@ def save_qualifying_loans(qualifying_loans):
                     no_path = False
                     save_csv(csvpath, qualifying_loans)
                 else:
-                    print("Incorrect file format. Save the file as .csv")
+                    print("Incorrect file format. Save the file as .csv")  
 
-
-
-    
 
 def run():
     """The main function for running the script."""
